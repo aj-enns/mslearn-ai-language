eaf4b9e9abb1ff4408838df41cd0a13e80400530
AJ Enns <ajenns@microsoft.com>
2024-03-10 22:09:40 -0500

    moving to forked repo

--- Labfiles/01-analyze-text/C-Sharp/text-analysis/obj/Debug/net7.0/text-analysis.AssemblyInfo.cs
+++ Labfiles/01-analyze-text/C-Sharp/text-analysis/obj/Debug/net7.0/text-analysis.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("text-analysis")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19da95722c2ac8064de3404c7afebb078a724d01")]
 [assembly: System.Reflection.AssemblyProductAttribute("text-analysis")]
 [assembly: System.Reflection.AssemblyTitleAttribute("text-analysis")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Labfiles/03-language/C-Sharp/clock-client/obj/Debug/net7.0/clock_client.AssemblyInfo.cs
+++ Labfiles/03-language/C-Sharp/clock-client/obj/Debug/net7.0/clock_client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("clock_client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19da95722c2ac8064de3404c7afebb078a724d01")]
 [assembly: System.Reflection.AssemblyProductAttribute("clock_client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("clock_client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
